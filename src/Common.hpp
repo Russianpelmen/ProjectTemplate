df86d78a118758fb843e6272d7e1e49a436bff7f
Russianpelmen <145644921+Russianpelmen@users.noreply.github.com>
2025-02-05 08:08:25 +0400

    Add files via upload

---
+++ src/Common.hpp
@@ -0,0 +1,6 @@
+﻿#pragma once
+
+#include <iostream>
+#include <windows.h>
+
+#include "..\Resource\resource.h"
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+﻿#include "Common.hpp"
+
+using namespace std;
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
+	
+	while (true) if (MessageBox(NULL, "Hello World!", "Press 'OK' to leave", MB_OKCANCEL) == MB_OKCANCEL) break;
+	
+	return 0;
+}
